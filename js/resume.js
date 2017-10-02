702ea985e87efc0237f02f23db0c6b24fccbc493
Zay19 <zbryantshaw@gmail.com>
2017-10-02 17:00:02 -0500

    recently created repository

---
+++ js/resume.js
@@ -0,0 +1,57 @@
+
+
+var str1 = "ZaShonne Bryant-Shaw.";  
+var str2 = str1.toUpperCase();  
+document.write(str2);
+console.log('Name:' + str2)
+
+var myCareer = 'Astronaut';
+console.log('Career field:' + myCareer)
+
+
+
+function displayPosition(myPast, companyName1, jobTitle1, description1, companyName2, jobTitle2, description2) {
+   displayPosition(myPast)
+displayPosition( companyName1 + jobTitle1, description1)
+displayPosition( companyName2 + jobTitle2, description2)
+};
+
+console.log(myPast= 'My Past Experience:')
+console.log(companyName1= '*' + 'StoreHouse-SelfStorage')
+console.log(jobTitle1= '-' + 'Maintenance')
+console.log(description1='-' + 'helped customers move in/out of their storages; keept grounds and storages clean')
+
+console.log(companyName2= '*' + 'Waffle House')
+console.log(jobTitle2= '-' + 'Grill-operator')
+console.log(description2='-' + 'cooked food')
+
+
+
+
+
+
+ 
+
+
+var myDescription = 'I am a 19 year old African-American. Graduated in May 2016. I only attended college for 1 year and immediatly left. Also I have dreads.'
+var myInterest = 'Video games, Skating, Anime, Music, Drawing';
+
+var mySkills = 'Check it: coding; Drawing; Check it: handling money; Command prompt'
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('Description:' + myDescription)
+console.log('My Interests:' + myInterest)
+
+console.log('My Skills:' + mySkills)
+
